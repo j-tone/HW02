78885638bd7c5434cee020e588685655b706a875
j-tone <jtoney17@my.whitworth.edu>
2014-09-16 21:26:45 -0700

    1.6 Done

--- PhoneDirectory/Array_Based_PD.h
+++ PhoneDirectory/Array_Based_PD.h
@@ -53,11 +53,28 @@ public:
 	void save();
 
 private:
-	class Directory_Entry /* Exercise 1.6: Please complete the definition of the Directory_Entry class here. - Ed/Kent */
+	class Directory_Entry
 	{
 	public:
-	
+		//Justin Toney
+		Directory_Entry() {} // Default no-argument constructor 
+		Directory_Entry(std::string the_name, std::string the_number) {
+			this->name = the_name;
+			this->number = the_number;
+		}
+		std::string get_name() const {
+			return name;
+		}
+		std::string get_number() const {
+			return number;
+		}
+		void set_number(const std::string& new_number) {
+			number = new_number;
+		}
 	private:
+		string name;
+		string number;
+	};
 
 	};
 
